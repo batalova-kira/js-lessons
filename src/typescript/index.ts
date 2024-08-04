b8d61bed4cb6d08891c33c17a513fae7617f3e87
batalova-kira <batalovakira@gmail.com>
2024-08-04 23:08:54 +0300

    add ts enum and interface

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,3 +1,5 @@
+import { log } from "console"
+
 let number: number = 5
 let person: string = "Alex"
 
@@ -10,3 +12,33 @@ const error = (message: string): never => {
 };
 
 let userName: null | string | undefined = "Mary"
+
+const numbers: Array<number> = [1, 2, 3, 4, 5, 6]
+
+const string: number[] = [1, 2, 3, 4, 5, 6]
+
+const names: Array<number | boolean | string> = [1, 2, 3, 4, 5, 6]
+
+interface IUser {
+    name: string;
+    age: number;
+    isActive: boolean;
+    location: string;
+};
+
+let user:IUser = {
+    name: "Mary",
+    age: 25,
+    isActive: true,
+    location: "Brazil"
+}
+
+user.age = 35
+
+enum Statuses {
+    Created = "Account created successfully",
+    NotFound = "User with email not found",
+    BadRequest = "The entered data is not correct"
+}
+
+ console.log(Statuses.Created)
